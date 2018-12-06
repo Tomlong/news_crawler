f63cc572a976f9b27d8653f479382b79f347deab
Terry Lin <terrylin@emotibot.com>
2018-12-06 13:15:35 +0800

    upload list_crawler & news_crawler

---
+++ list_crawler.py
@@ -0,0 +1,66 @@
+import os
+import ast
+import requests
+import json
+import time
+
+
+def build_dic(dic):
+    new_dic = {}
+    for now_dic in dic:
+        key = now_dic['id']
+        new_dic[key] = {}
+        new_dic[key]['url'] = now_dic['url']
+        new_dic[key]['date'] = now_dic['date']
+        new_dic[key]['title'] = now_dic['title']
+    return new_dic    
+
+def update_news_list(now_news_dic):
+    if not(os.path.isfile('news_list.json')):
+        jsonFile = open("news_list.json", "w")
+        jsonFile.write(json.dumps(now_news_dic))
+        jsonFile.close()
+        
+        f = open('jobs_list.txt','a')
+        for key in now_news_dic:
+            f.write(key+'\n')
+        f.close()
+        return
+    else:
+        news_list = json.loads(open('news_list.json').read())
+    update_list = set(now_news_dic.keys())-set(news_list.keys()) 
+    print (update_list)
+    #Update news list
+    for key in update_list:
+        news_list[key]['url'] = now_news_dic['key']['url']
+        news_list[key]['date'] = now_news_dic['key']['date']
+        news_list[key]['title'] = now_news_dic['key']['title']
+        
+    news_list_json = json.dumps(news_list)
+    f = open('news_list.json','w')
+    f.write(news_list_json)
+    f.close()
+    
+    #Update job list
+    f = open('jobs_list.txt','a')
+    for key in update_list:
+        f.write(key+'\n')
+    f.close()
+    
+def list_crawler():
+    now_time = int(time.time()*1000)
+    now_news_list = requests.get('http://news.people.com.cn/210801/211150/index.js?_='+str(now_time))
+    now_news_dic = ast.literal_eval(now_news_list.text)['items']
+    #print (now_news_dic)
+    now_news_dic = build_dic(now_news_dic)
+    #print (now_news_dic)
+    update_news_list(now_news_dic)
+
+def main():
+    while 1:
+        list_crawler()
+        #sleep 20 miniutes
+        time.sleep(1200)
+
+if __name__ == '__main__':
+    main()
---
+++ news_crawler.py
@@ -0,0 +1,36 @@
+
+import os
+import ast
+import requests
+import json
+import time
+
+def read_and_delete_job()  
+    job = open('jobs_list.txt','r')
+    jobs = job.readlines()
+    now_job = jobs[0].rstrip('\n')
+    del jobs[0]
+    job = open('jobs_list.txt','w')
+    job.writelines(jobs)
+    job.close()
+    return now_job
+
+def news_crawler():
+    news_list = json.loads(open('news_list.json').read())
+    job_id = read_and_delete_job()
+    if job_id =='':
+        return False
+    print("Crawl ",job_id)
+    html = requests.get(news_list[job_id]['url']).text
+    file = open('news_html/'+job_id+'.txt','w')
+    file.writelines(html)
+    file.close()
+    return True
+def main():
+    
+    while news_crawler():
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
+
